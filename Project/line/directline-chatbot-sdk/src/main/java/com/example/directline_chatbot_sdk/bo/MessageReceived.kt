4f4dea98aef77d24785933f280f92711d655c61c
Abhinav <abhinavkumar17@gmail.com>
2021-09-22 17:41:37 +0530

    fix

--- Project/line/directline-chatbot-sdk/src/main/java/com/example/directline_chatbot_sdk/bo/MessageReceived.kt
+++ Project/line/directline-chatbot-sdk/src/main/java/com/example/directline_chatbot_sdk/bo/MessageReceived.kt
@@ -2,7 +2,7 @@ package com.example.directline_chatbot_sdk.bo
 
 import java.io.Serializable
 
-internal data class MessageReceivedX
+internal data class MessageReceived
     (
     val activities: List<MessageActivity>,
     val watermark: String
