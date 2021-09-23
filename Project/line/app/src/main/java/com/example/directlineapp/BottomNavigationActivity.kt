03e1f4a2d57dd63dcb3bf9b771c9b117a3bcfd75
Akshay Kiran Bhalerao <bhaleraoakshay350@gmail.com>
2021-09-23 15:43:04 +0530

    test branch

--- Project/line/app/src/main/java/com/example/directlineapp/BottomNavigationActivity.kt
+++ Project/line/app/src/main/java/com/example/directlineapp/BottomNavigationActivity.kt
@@ -71,8 +71,7 @@ class BottomNavigationActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_bootom_navigation)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
